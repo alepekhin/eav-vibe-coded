f15afad0fdc7f19aec43263e958a02216b53f70e
Alexander Lepekhin <alepekhine@gmail.com>
2025-09-03 20:10:20 +0300

    initial commit

---
+++ src/main/java/com/gmail/alepekhine/eav/EavAiVibeApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.alepekhine.eav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EavAiVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EavAiVibeApplication.class, args);
+	}
+
+}
