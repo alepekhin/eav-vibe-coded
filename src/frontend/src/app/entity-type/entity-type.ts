bd6e8433bd611eab7103ed5be05f9ff0db7eade1
Alexander Lepekhin <alepekhine@gmail.com>
2025-09-12 20:41:16 +0300

    material table with paging, sorting and filtering

--- src/frontend/src/app/entity-type/entity-type.ts
+++ src/frontend/src/app/entity-type/entity-type.ts
@@ -4,27 +4,55 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { EntityTypeService, EntityType } from './entity-type-service';
+import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CdkColumnDef } from '@angular/cdk/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { ViewChild } from '@angular/core'
+//import { BrowserModule } from '@angular/platform-browser';    
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-entity-type',
   imports: [
     CommonModule,  
     FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginator,
+    MatSortModule,
+   // BrowserModule,
+   // BrowserAnimationsModule,
   ],
   templateUrl: './entity-type.html',
-  styleUrls: ['./entity-type.css']
+  styleUrls: ['./entity-type.css'],
+  providers: [CdkColumnDef],
 })
 export class EntityTypeComponent implements OnInit {
 
   entityTypes: EntityType[] = [];
   selectedEntityType: EntityType = {id: 0, name: ''};
   newEntityType: EntityType = { name: '' };
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource = new MatTableDataSource<EntityType>(this.entityTypes);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private entityTypeService: EntityTypeService) { }
 
 
   ngOnInit(): void {
     this.getAllEntityTypes();
+  }
+  
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   selectEntityType(entityType: EntityType) {
@@ -35,6 +63,11 @@ export class EntityTypeComponent implements OnInit {
   getAllEntityTypes(): void {
     this.entityTypeService.getAll().subscribe(data => {
       this.entityTypes = data;
+      setTimeout( () => {
+        this.dataSource = new MatTableDataSource<EntityType>(this.entityTypes);
+        this.dataSource.paginator = this.paginator; 
+        this.dataSource.sort = this.sort;
+      }, 1000  );
     });
   }
 
@@ -61,6 +94,11 @@ export class EntityTypeComponent implements OnInit {
         this.getAllEntityTypes();
       });
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
